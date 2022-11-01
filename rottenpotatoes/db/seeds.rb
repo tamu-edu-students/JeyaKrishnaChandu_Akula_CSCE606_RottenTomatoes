bcc283d0723b121378908ecd6e365753bb18bb52
chanduakula-tamu <chanduakula@tamu.edu>
2022-10-31 20:06:48 -0500

    Final Upload - HW2

--- rottenpotatoes/db/seeds.rb
+++ rottenpotatoes/db/seeds.rb
@@ -17,7 +17,13 @@ more_movies = [
     {:title => 'Nomadland', :rating => 'R',
       :release_date => '19-Feb-2021'},
     {:title => 'CODA', :rating => 'PG-13',
-      :release_date => '13-Aug-2021'}
+      :release_date => '13-Aug-2021'},
+    {:title => 'Money Heist', :rating => 'R',
+      :release_date => '15-May-2019'},
+    {:title => 'Prison Break', :rating => 'R',
+      :release_date => '01-Feb-2020'},
+    {:title => 'Breaking Bad', :rating => 'PG-13',
+      :release_date => '13-Aug-2015'}
   ]
   
   more_movies.each do |movie|
